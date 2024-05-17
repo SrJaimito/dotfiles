e7de1af08e307bd49e549157e58413b00efd7ea5
SrJaimito <jaime.senors@gmail.com>
2024-05-17 13:37:29 +0200

    Create install and update scripts

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+function ask_install {
+    echo -n "[*] $1 [Y/n]"
+    read -n 1 -r -s ANSWER
+
+    echo -e -n "\r [*] $1 "
+
+    if [[ $ANSWER =~ ^[Yy]$ || -z $ANSWER ]]
+    then
+        echo -e "\xE2\x9C\x94    "
+        return 0
+    else
+        echo -e "\xE2\x9D\x8C    "
+        return 1
+    fi
+}
+
+
+echo "Select installation:"
+
+if ask_install Neovim
+then
+    cp -r .config/nvim ~/.config/nvim
+fi
+
