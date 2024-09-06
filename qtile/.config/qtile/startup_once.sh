e4387f219c6d852d34df35eb521fb91fb2c7eaed
SrJaimito <jaime.senors@gmail.com>
2024-09-06 10:50:49 +0200

    Update btop theme

--- qtile/.config/qtile/startup_once.sh
+++ qtile/.config/qtile/startup_once.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# Configure monitors
+xrandr --output DP-3 --mode 1920x1080 --rate 60 --primary
+xrandr --output HDMI-3 --mode 1920x1080 --rate 60 --left-of DP-3
+
 # Set keyboard layout, just in case
 setxkbmap -layout es
 
