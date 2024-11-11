84282fa071bfb5fb23349f74b5ec652623180de5
SrJaimito <jaime.senors@gmail.com>
2024-11-11 19:32:02 +0100

    Correctly allow telescope to search and grep hidden files excluding .git folders

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,13 +7,22 @@ return {
     },
 
     config = function()
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    file_ignore_patterns = { 'node_modules', '.git', '.venv' }
+                },
+                live_grep = {
+                    additional_args = { '--hidden' },
+                    file_ignore_patterns = { 'node_modules', '.git', '.venv' }
+                }
+            }
+        }
+
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', function()
-            builtin.find_files({ hidden = true })
-        end)
-        vim.keymap.set('n', '<leader>fg', function()
-            builtin.live_grep({ hidden = true })
-        end)
+        vim.keymap.set('n', '<leader>ff', builtin.find_files)
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep)
         vim.keymap.set('n', '<leader>fb', builtin.buffers)
         vim.keymap.set('n', '<leader>fh', builtin.help_tags)
     end
