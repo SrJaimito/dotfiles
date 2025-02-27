23ceebfa1e91b72f1fe7080c0590bbce7e9f4358
SrJaimito <jaime.senors@gmail.com>
2025-02-27 22:47:45 +0100

    Change workspace module to internal/i3

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,6 +3,9 @@
 
 polybar-msg cmd quit
 
-echo "---" | tee -a /tmp/polybar_primary.log
-polybar primary 2>&1 | tee -a /tmp/polybar_primary.log & disown
+echo "---" | tee -a /tmp/polybar_workspace.log
+polybar workspace_bar 2>&1 | tee -a /tmp/polybar_workspace.log & disown
+
+echo "---" | tee -a /tmp/polybar_info.log
+polybar info_bar 2>&1 | tee -a /tmp/polybar_info.log & disown
 
