da3c19ff62d58d57cabec13e14640f469e6155a4
SrJaimito <jaime.senors@upm.es>
2026-05-10 10:23:32 +0200

    Update loop for lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,7 +41,7 @@ vim.keymap.set('n', '<A-k>', 'ddkkp')
     -------------
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         'git',
         'clone',
